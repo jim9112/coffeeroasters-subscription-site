f4bb66fe5a44d4ecbc7bf35bd9859d4abe791787
jim9112 <jim9112@gmail.com>
2021-02-03 15:29:46 -0500

    mobile header and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      body: 'Barlow, sans-serif',
+      heading: 'Fraunces, serif',
+    },
+    colors: {
+      'dark-cyan': '#0E8784',
+      'dark-grey-blue': '#333D4B',
+      'light-cream': '#FEFCF7',
+      grey: '#83888F',
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        'hero-small': "url('assets/home/mobile/image-hero-coffeepress.jpg')",
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
