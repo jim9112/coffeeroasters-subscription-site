cdeb629f76cad5764ac98f3227363337f2a5d7f0
jim9112 <jim9112@gmail.com>
2021-02-16 16:24:52 -0500

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: { enabled: true, content: ['*.html', 'js/*.js'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
